b87c72e4fd40066308690f6104ef4a75dfa93457
Josh Lemon <josh@joshlemon.com.au>
2024-12-30 17:08:39 +1100

    logo updates

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,7 @@ templates_path = ['_templates']
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ["_static"]
 
-html_logo = "img/Indigo-430-x-518"
+html_logo = "img/indigo-logo.png"
 html_theme_options = {
     "logo_only": True,
 }
